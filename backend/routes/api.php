01e81d724b65e6d06eb33dde8ecd042fe4ae172c
Sean McNamara <me@seanmcn.com>
2021-03-07 14:59:56 +0000

    Removes the 'Book' example of auth over the API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,5 +18,3 @@ use App\Http\Controllers;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::middleware('auth:sanctum')->get('/book', [Controllers\BookController::class, 'index']);
