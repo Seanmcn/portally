66c6dcf8fe5cf57ae3fadd9f3e650b2c89a42254
Sean McNamara <me@seanmcn.com>
2021-03-07 15:01:40 +0000

    Moves axios `withCredentials` to `defaults`

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+
 const api = axios.create({
   baseURL: 'https://api.circle.devel',
-  withCredentials: true,
 });
+
 
 export default api;
